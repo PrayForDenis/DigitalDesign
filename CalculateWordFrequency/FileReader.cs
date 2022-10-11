dc3162168ae6c6a2610682989f8097f34e5f3860
Denis <90082770+PrayForDenis@users.noreply.github.com>
2022-10-11 21:48:56 +0300

    2 homework

---
+++ CalculateWordFrequency/FileReader.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+
+namespace CalculateWordFrequency
+{
+    public class FileReader : ITextReader
+    {
+        private readonly string _name;
+
+        public FileReader(string name)
+        {
+            _name = name;
+        }
+
+        public string Read()
+        {
+            string content;
+
+            try
+            {
+                content = File.ReadAllText(_name, System.Text.Encoding.UTF8);
+            }
+            catch (FileNotFoundException e)
+            {
+                Console.WriteLine("Error: {0}", e.Message);
+                return "";
+            }
+
+            return content;
+        }
+    }
+}
---
+++ CalculateWordFrequency/IWriter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculateWordFrequency
+{
+    public interface IWriter
+    {
+        void Write(IReadOnlyDictionary<string, int> frequency);
+    }
+}
